8976e215f81f26f6a6e9034b67203d9e4a3886dc
Sly <cri21631@uvg.edu.gt>
2022-10-20 23:47:58 -0600

    4Dogs v1.4.4

--- 4Dogs/app/src/main/java/com/example/fordogs/MainActivity.kt
+++ 4Dogs/app/src/main/java/com/example/fordogs/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.fordogs.databinding.ActivityMainBinding
 import com.example.fordogs.ui.fragments.calendar.CalendarFragment
+import com.example.fordogs.ui.fragments.events.EventsFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +23,26 @@ class MainActivity : AppCompatActivity() {
 
         binding.bottomNavigationView.background = null
         binding.bottomNavigationView.menu.getItem(2).isEnabled = false
+        setListeners()
+    }
 
+    private fun setListeners() {
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.bottom_nav_calendar -> replaceFragment(CalendarFragment())
+                R.id.bottom_nav_dogs -> replaceFragment(EventsFragment())
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.main_fragment_view, fragment)
+        }
     }
 
 }
