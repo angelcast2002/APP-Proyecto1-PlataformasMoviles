5d8dc305685c8dc926299f2b4a7808f88e89cf9a
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2022-10-21 11:49:33 -0600

    4Dogs v1.5.3

--- 4Dogs/app/src/main/java/com/example/fordogs/ui/fragments/editProfile/EditProfileFragment.kt
+++ 4Dogs/app/src/main/java/com/example/fordogs/ui/fragments/editProfile/EditProfileFragment.kt
@@ -7,10 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
+import coil.load
+import coil.request.CachePolicy
+import coil.transform.CircleCropTransformation
+import com.example.fordogs.R
 import com.example.fordogs.databinding.EditProfileLayoutBinding
 import com.example.fordogs.databinding.RegisterLayoutBinding
+import com.example.fordogs.ui.fragments.editProfile.EditProfileViewModel
+import com.example.fordogs.ui.fragments.editProfile.EditProfileViewModel.Status.*
 import com.example.fordogs.ui.fragments.viewModelActivity.RegisterToLoginViewModel
 import com.example.fordogs.ui.util.BaseFragment
 import kotlinx.coroutines.flow.collectLatest
@@ -18,7 +25,8 @@ import kotlinx.coroutines.launch
 
 class EditProfileFragment: BaseFragment<EditProfileLayoutBinding>(){
     private val RegisterToLoginViewModel: RegisterToLoginViewModel by activityViewModels()
-    
+    private val EditProfileViewModel: EditProfileViewModel by viewModels()
+
     lateinit var name: String
 
     override fun getViewBinding() = EditProfileLayoutBinding.inflate(layoutInflater)
@@ -32,6 +40,7 @@ class EditProfileFragment: BaseFragment<EditProfileLayoutBinding>(){
         setNameEditText()
 
 
+
     }
 
     private fun setObservables() {
@@ -39,6 +48,29 @@ class EditProfileFragment: BaseFragment<EditProfileLayoutBinding>(){
             RegisterToLoginViewModel.name.collectLatest {
                 name = RegisterToLoginViewModel.name.value
             }
+        }
+
+        lifecycleScope.launch {
+            EditProfileViewModel.imagen.collectLatest { img ->
+                setImgUser(img)
+            }
+        }
+
+        lifecycleScope.launch {
+            EditProfileViewModel.status.collectLatest{ status ->
+                handleStatus(status)
+            }
+        }
+
+    }
+
+    private fun setImgUser(img: String) {
+        binding.imgPetEditProfileLayout.load(img){
+            transformations(CircleCropTransformation())
+            diskCachePolicy(CachePolicy.ENABLED)
+            memoryCachePolicy(CachePolicy.ENABLED)
+            error(R.drawable.ic_error)
+            placeholder(R.drawable.ic_download)
         }
     }
 
@@ -48,9 +80,49 @@ class EditProfileFragment: BaseFragment<EditProfileLayoutBinding>(){
 
     private fun setListeners() {
         binding.btSaveDataEditProfale.setOnClickListener{
-            requireView().findNavController().navigate(
-                EditProfileFragmentDirections.actionEditProfileFragmentToCalendarFragment()
-            )
+            EditProfileViewModel.saveChanges()
+        }
+    }
+
+    fun handleStatus(status: EditProfileViewModel.Status) {
+        when(status){
+            Editing -> {
+                binding.apply {
+                    imgPetEditProfileLayout.visibility = View.VISIBLE
+                    petNameEdtiProfileLayout.visibility = View.VISIBLE
+                    textInputRazaTextEditProfileLayout.visibility = View.VISIBLE
+                    textInputPesoTextEditProfileLayout.visibility = View.VISIBLE
+                    textInputColorTextEditProfileLayout.visibility = View.VISIBLE
+                    textInputAlturaTextEditProfileLayout.visibility = View.VISIBLE
+                    textInputLargoTextEditProfileLayout.visibility = View.VISIBLE
+                    textInputComidaFavTextEditProfileLayout.visibility  = View.VISIBLE
+                    btSaveDataEditProfale.visibility = View.VISIBLE
+                    progressEditLayout.visibility = View.GONE
+                }
+            }
+            is Error -> {
+                TODO()
+            }
+            Loading -> {
+                binding.apply {
+                    imgPetEditProfileLayout.visibility = View.VISIBLE
+                    petNameEdtiProfileLayout.visibility = View.VISIBLE
+                    textInputRazaTextEditProfileLayout.visibility = View.GONE
+                    textInputPesoTextEditProfileLayout.visibility = View.GONE
+                    textInputColorTextEditProfileLayout.visibility = View.GONE
+                    textInputAlturaTextEditProfileLayout.visibility = View.GONE
+                    textInputLargoTextEditProfileLayout.visibility = View.GONE
+                    textInputComidaFavTextEditProfileLayout.visibility  = View.GONE
+                    btSaveDataEditProfale.visibility = View.GONE
+                    progressEditLayout.visibility = View.VISIBLE
+                }
+            }
+            Succes -> {
+                requireView().findNavController().navigate(
+                    EditProfileFragmentDirections.actionEditProfileFragmentToCalendarFragment()
+                )
+                EditProfileViewModel.setDefaul()
+            }
         }
     }
 
---
+++ 4Dogs/app/src/main/java/com/example/fordogs/ui/util/EditProfileConstants.kt
@@ -0,0 +1,14 @@
+package com.example.fordogs.ui.util
+
+class EditProfileConstants {
+
+    companion object {
+        const val RAZA = "Chihuahua"
+        const val PESO = 10
+        const val COLOR = "Cafe"
+        const val ALTO = 10
+        const val LARGO = 25
+        const val COMIDA_FAV = "Pizza"
+        const val IMG_PERRO = "https://www.petdarling.com/wp-content/uploads/2020/11/razas-de-perros.jpg"
+    }
+}
