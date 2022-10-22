459132a2d980c2e167d803fd7489d510a23608e5
Sly <cri21631@uvg.edu.gt>
2022-10-21 20:00:01 -0600

    $Dogs v1.5.5

--- 4Dogs/app/src/main/java/com/example/fordogs/MainActivity.kt
+++ 4Dogs/app/src/main/java/com/example/fordogs/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.example.fordogs
 
-import android.app.ProgressDialog.show
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.example.fordogs.databinding.ActivityMainBinding
-import com.example.fordogs.ui.fragments.addevents.AddEventFragment
-import com.example.fordogs.ui.fragments.calendar.CalendarFragment
-import com.example.fordogs.ui.fragments.events.EventsFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -64,13 +55,13 @@ class MainActivity : AppCompatActivity() {
                     navController.navigate(R.id.eventsFragment)
                     true
                 }
-                R.id.bottom_nav_settings ->{
+                R.id.bottom_nav_logout ->{
 
                     val builder = AlertDialog.Builder(this)
                     builder.apply {
                         setTitle(getString(R.string.text_Advertencia))
                         setMessage(getString(R.string.text_mensaje_cerrarSesion))
-                        setPositiveButton(getString(R.string.text_Eliminar)
+                        setPositiveButton(getString(R.string.text_Si)
                         ) { _, _ ->
                             navController.navigate(R.id.loginFragment)
                             finishAffinity() //Finaliza la app
