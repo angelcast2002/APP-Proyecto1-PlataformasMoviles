175597778967bfe408597a6b0d20b15f94cb94f3
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2022-10-30 12:20:49 -0600

    4Dogs 1.8.1

--- 4Dogs/app/src/main/java/com/example/fordogs/ui/fragments/profile/ProfileViewModel.kt
+++ 4Dogs/app/src/main/java/com/example/fordogs/ui/fragments/profile/ProfileViewModel.kt
@@ -1,4 +1,44 @@
 package com.example.fordogs.ui.fragments.profile
 
-class ProfileViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.fordogs.data.Resource
+import com.example.fordogs.data.local.entity.userPerro
+import com.example.fordogs.data.repository.UserPerroRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val repository: UserPerroRepository
+): ViewModel() {
+
+    private val _status = MutableStateFlow<Status>(Status.Loading)
+    val status : StateFlow<Status> = _status
+
+
+    sealed class Status{
+        class Succes(val data: userPerro): Status()
+        object Loading: Status()
+        class Error(val message: String): Status()
+    }
+
+    fun getData(){
+        viewModelScope.launch {
+            _status.value = Status.Loading
+            when(val perroInfoResult = repository.getUserPerroInfo()){
+                is Resource.Succes -> {
+                    _status.value = Status.Succes(perroInfoResult.data!!)
+                }
+                is Resource.Error -> {
+                    _status.value = Status.Error(perroInfoResult.message!!)
+                }
+            }
+        }
+    }
+
 }
