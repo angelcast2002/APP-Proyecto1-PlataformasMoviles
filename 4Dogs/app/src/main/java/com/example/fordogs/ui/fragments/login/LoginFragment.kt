6818335b718d1fdd9858517abe2eb1540bc21a94
Javier Alejandro Ramírez Heredia <ram21600@uvg.edu.gt>
2022-11-19 17:25:11 -0600

    Loging Fragment Fix
    Loging Fragment Fix

--- 4Dogs/app/src/main/java/com/example/fordogs/ui/fragments/login/LoginFragment.kt
+++ 4Dogs/app/src/main/java/com/example/fordogs/ui/fragments/login/LoginFragment.kt
@@ -1,35 +1,33 @@
 package com.example.fordogs.ui.fragments.login
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
-import com.example.fordogs.data.repository.Firebase.FirebaseRepository
-import com.example.fordogs.data.repository.Firebase.FirebaseRepositoryImpl
+import com.example.fordogs.R
 import com.example.fordogs.databinding.LoginLayoutBinding
 import com.example.fordogs.ui.util.BaseFragment
 import com.example.fordogs.ui.fragments.login.LoginViewModel.*
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class LoginFragment: BaseFragment<LoginLayoutBinding>(){
     private val logInViewModel: LoginViewModel by viewModels()
 
-    private lateinit var firebaseRepository: FirebaseRepository
-    private lateinit var email: String
+
+    private lateinit var correo: String
     private lateinit var password: String
     override fun getViewBinding() = LoginLayoutBinding.inflate(layoutInflater)
 
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        firebaseRepository=FirebaseRepositoryImpl(
-            api = FirebaseAuthApiImplementation()
-        )
 
         hideNavBar()
         setListeners()
@@ -92,25 +90,12 @@ class LoginFragment: BaseFragment<LoginLayoutBinding>(){
         }
     }
 
-    private fun setListeners(status: LoginViewModel.Status) {
+    private fun setListeners() {
         binding.btIniciarSesionLoginLayout.setOnClickListener{
 
-            val email= binding.textInputCorreoTextLoginlayoutEditText.text.toString()
-            val password = binding.textInputPasswordTextLoginLayoutEditText.text.toString()
-            logInViewModel.loading(email, password)
-
-            lifecycleScope.launch() {
-                val id = firebaseRepository.signInWithEmailAndPasword(email, password)
-                if(id != null){
-                    Toast.makeText(activity, status.message,
-                        Toast.LENGTH_LONG)
-                        .show()
-                }else{
-                    Toast.makeText(activity, status.message,
-                        Toast.LENGTH_LONG)
-                        .show()
-                }
-            }
+            correo = binding.textInputCorreoTextLoginlayoutEditText.text.toString()
+            password = binding.textInputPasswordTextLoginLayoutEditText.text.toString()
+            logInViewModel.loading(correo, password)
         }
 
         binding.btSignUpLoginLayout.setOnClickListener {
