baa2b8f6d34a73c5bd3cc5dfd76b066270c8e71a
Javier Alejandro Ramírez Heredia <ram21600@uvg.edu.gt>
2022-11-19 17:21:32 -0600

    Firebase
    Firebase Api

--- 4Dogs/app/src/main/java/com/example/fordogs/data/remote/firebase/FirebaseApiImpl.kt
+++ 4Dogs/app/src/main/java/com/example/fordogs/data/remote/firebase/FirebaseApiImpl.kt
@@ -2,45 +2,42 @@ package com.example.fordogs.data.remote.firebase
 
 import com.example.fordogs.data.Resource
 import com.example.fordogs.data.remote.FirebaseApi
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.tasks.await
 
-class FirebaseApiImpl: FirebaseApi {
+class FirebaseApiImpl(private val api: FirebaseAuth): FirebaseApi {
     override suspend fun signInWithEmailAndPasword(email: String, password: String): Resource<String> {
 
-        val auth = Firebase.auth
-
-
-        auth.signInWithEmailAndPassword(email, password).await()
-        val userInfo = response.user
-
         return try {
-            if (userInfo != null) {
-                Resource.Success(data = userInfo.uid)
-            } else {
+            val response = api.signInWithEmailAndPassword(email, password).await()
+
+            val user = response.user
+            if (user != null)
+                Resource.Success(data = user.uid)
+            else
                 Resource.Error(message = "User not found")
-            }
         } catch (e: Exception) {
             Resource.Error(message = "User not found")
         }
 
+
+
     }
 
     override suspend fun signUpWithEmailAndPasword(email: String, password: String): Resource<String> {
-        val auth = Firebase.auth
-
-        auth.createUserWithEmailAndPassword(email, password).await()
-        val userInfo = response.user
-
         return try {
-            if (userInfo != null) {
-                Resource.Success(data = userInfo.uid)
-            } else {
+            val response = api.createUserWithEmailAndPassword(email, password).await()
+
+            val user = response.user
+            if (user != null)
+                Resource.Success(data = user.uid)
+            else
                 Resource.Error(message = "User not created")
-            }
         } catch (e: Exception) {
-            Resource.Error(message = "User not Created")
+            Resource.Error(message = "User not created")
         }
+
     }
 }
