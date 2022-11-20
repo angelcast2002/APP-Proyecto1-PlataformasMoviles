c73fdafc7cdc67e1e7d53f6580cdb827901eb48a
Sly <cri21631@uvg.edu.gt>
2022-11-20 15:22:36 -0600

    Revert "update"
    This reverts commit 43cfd923c1f5b78e6fdecfd54429961fe8002891.

--- 4Dogs/app/src/main/java/com/example/fordogs/data/repository/Firestore/FirestoreRepository.kt
+++ 4Dogs/app/src/main/java/com/example/fordogs/data/repository/Firestore/FirestoreRepository.kt
@@ -4,7 +4,7 @@ import com.example.fordogs.data.Resource
 import com.example.fordogs.data.local.entity.UserPerro
 
 interface FirestoreRepository {
-    suspend fun getUserPerroInfo(data: UserPerro): Resource<UserPerro>
+    suspend fun getUserPerroInfo(): Resource<UserPerro>
     suspend fun setUserPerroInfo(data: UserPerro): Resource<UserPerro>
 
 }
