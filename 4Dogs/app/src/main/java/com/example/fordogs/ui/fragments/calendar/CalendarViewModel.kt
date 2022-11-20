47a506145430ae7982b50ed9f582bf73e9ca96da
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2022-11-19 23:16:24 -0600

    Revert "Merge branch 'main' into apiImplementation2"
    This reverts commit 560d6a35e15c66be32d3211e3f11eae1e9018dde, reversing
    changes made to 76e5adaf0da14464a7d3560c1a3f77319e6e1352.

--- 4Dogs/app/src/main/java/com/example/fordogs/ui/fragments/calendar/CalendarViewModel.kt
+++ 4Dogs/app/src/main/java/com/example/fordogs/ui/fragments/calendar/CalendarViewModel.kt
@@ -1,33 +1,43 @@
 package com.example.fordogs.ui.fragments.calendar
 
-import android.os.Build
-import androidx.annotation.RequiresApi
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.fordogs.data.Resource
+import com.example.fordogs.data.local.entity.Event
 import com.example.fordogs.data.local.entity.UserPerro
+import com.example.fordogs.data.repository.eventosRepo.EventRepository
 import com.example.fordogs.data.repository.userPerroRepo.UserPerroRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import java.time.LocalDate
-import java.time.YearMonth
-import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 @HiltViewModel
 class CalendarViewModel @Inject constructor(
-    private val repository: UserPerroRepository
+    private val repository: UserPerroRepository,
+    private val eventRepository: EventRepository
 ): ViewModel() {
 
     private val _status = MutableStateFlow<Status>(Status.Loading)
     val status : StateFlow<Status> = _status
 
+    private val _eventStatus = MutableStateFlow<EventStatus>(EventStatus.Loading)
+    val eventStatus : StateFlow<EventStatus> = _eventStatus
+
     sealed class Status{
         object Loading: Status()
         class Succes(val data: UserPerro): Status()
         class Error(val message: String): Status()
+    }
+
+    sealed class EventStatus {
+        object Loading: EventStatus()
+        class Success(val data: List<Event>): EventStatus()
+        class Deleted(val message: String): EventStatus()
+        class Error(val message: String): EventStatus()
     }
 
     fun getData(){
@@ -44,42 +54,30 @@ class CalendarViewModel @Inject constructor(
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun daysInMonthArray(date: LocalDate?): ArrayList<LocalDate?> {
-
-        val daysInMonthArray = arrayListOf<LocalDate?>()
-        val yearMonth = YearMonth.from(date)
-        val daysInMonth = yearMonth.lengthOfMonth()
-
-        val firstOfMonth = CalendarConstants.selectedDate?.withDayOfMonth(1)
-        val dayOfWeek = firstOfMonth?.dayOfWeek?.value
-
-        for (i in 1..42) {
-
-            if (i <= dayOfWeek!! || i > daysInMonth + dayOfWeek) {
-                daysInMonthArray.add(null)
-            } else {
-                daysInMonthArray.add(CalendarConstants.selectedDate?.let {
-                    LocalDate.of(
-                        it.year,
-                        CalendarConstants.selectedDate?.month,
-                        i - dayOfWeek
-                    )
-                })
+    fun getEvents() {
+        viewModelScope.launch {
+            _eventStatus.value = EventStatus.Loading
+            when(val eventsResult = eventRepository.getEvents()){
+                is Resource.Success -> {
+                    _eventStatus.value = EventStatus.Success(eventsResult.data!!)
+                }
+                is Resource.Error -> {
+                    _eventStatus.value = EventStatus.Error(eventsResult.message!!)
+                }
             }
-
         }
-
-        return daysInMonthArray
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun monthYearFromDate(date: LocalDate?): String {
-
-        val formatter = DateTimeFormatter.ofPattern("MMMM yyyy")
-
-        return date!!.format(formatter)
-    }
-
+    fun deleteEvent(eventId: Int) =
+        viewModelScope.launch {
+            when(val eventsResult = eventRepository.deleteEvent(eventId)){
+                is Resource.Success -> {
+                    _eventStatus.value = EventStatus.Deleted(eventsResult.data!!)
+                }
+                is Resource.Error -> {
+                    _eventStatus.value = EventStatus.Error(eventsResult.message!!)
+                }
+            }
+        }
 
 }
--- 4Dogs/app/src/main/java/com/example/fordogs/ui/fragments/events/EventsFragmentViewModel.kt
+++ 4Dogs/app/src/main/java/com/example/fordogs/ui/fragments/events/EventsFragmentViewModel.kt
@@ -4,7 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.fordogs.data.Resource
 import com.example.fordogs.data.local.entity.PerroTips
+import com.example.fordogs.data.local.entity.UserPerro
+import com.example.fordogs.data.remote.dto.PerroTipsRecyclerView
 import com.example.fordogs.data.repository.perroTipsRepo.PerroTipsRepository
+import com.example.fordogs.data.repository.userPerroRepo.UserPerroRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -13,33 +16,70 @@ import javax.inject.Inject
 
 @HiltViewModel
 class EventsFragmentViewModel @Inject constructor(
-    private val tips: PerroTipsRepository
+    private val tips: PerroTipsRepository,
+    private val repository: UserPerroRepository
 ): ViewModel() {
 
     private val _statusTips = MutableStateFlow<StatusTips>(StatusTips.Default)
     val statusTips: StateFlow<StatusTips> = _statusTips
 
+    private val _status = MutableStateFlow<Status>(Status.Loading)
+    val status : StateFlow<Status> = _status
+
     sealed class StatusTips{
         object Default: StatusTips()
+        object Loading: StatusTips()
         class Error(val message: String): StatusTips()
         class Succes(val data: PerroTips): StatusTips()
     }
 
-    fun prueba(name:String){
-        viewModelScope.launch {
+    sealed class Status{
+        class Succes(val data: UserPerro): Status()
+        object Loading: Status()
+        class Error(val message: String): Status()
+    }
 
+
+    fun getTips(name:String){
+        viewModelScope.launch {
+            _statusTips.value = StatusTips.Loading
             when(val resultado = tips.getPerroTips(name)) {
                 is Resource.Error -> {
                     _statusTips.value = StatusTips.Error(resultado.message!!)
                 }
                 is Resource.Success -> {
                     _statusTips.value = StatusTips.Succes(resultado.data!!)
+                    saveData(resultado.data)
                 }
             }
         }
     }
 
+    private fun saveData(data:PerroTips){
+        viewModelScope.launch {
+            tips.savePerroTips(data)
+        }
+    }
+
     fun pruebaToDefault(){
         _statusTips.value = StatusTips.Default
+    }
+
+    fun getData(){
+        viewModelScope.launch {
+            _status.value = Status.Loading
+            when(val perroInfoResult = repository.getUserPerroInfo()){
+                is Resource.Success -> {
+                    _status.value = Status.Succes(perroInfoResult.data!!)
+                }
+                is Resource.Error -> {
+                    _status.value = Status.Error(perroInfoResult.message!!)
+                }
+            }
+        }
+    }
+
+    fun setLoading(){
+        _status.value = Status.Loading
     }
 }
