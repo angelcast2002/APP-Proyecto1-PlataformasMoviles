7d68fe3829389b5b6919ccec5b567a6dc5f2e7d0
Sly <cri21631@uvg.edu.gt>
2022-11-21 15:31:42 -0600

    4Dogs v3..0.2
    Events fragment fast refresh

--- 4Dogs/app/src/main/java/com/example/fordogs/ui/fragments/calendar/CalendarFragment.kt
+++ 4Dogs/app/src/main/java/com/example/fordogs/ui/fragments/calendar/CalendarFragment.kt
@@ -69,10 +69,6 @@ class CalendarFragment : BaseFragment<FragmentCalendarBinding>(), EventOptionsLi
                 }
             }
             CalendarViewModel.EventStatus.Loading -> {
-                binding.apply {
-                    eventListProgressBar.visibility = View.VISIBLE
-                    recyclerConstraintLayout.visibility = View.GONE
-                }
             }
             is CalendarViewModel.EventStatus.Deleted -> {
                 binding.apply {
